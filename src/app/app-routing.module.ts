814ad56c84c14e70e95d6006eef0ef72456e6659
Gabriel <gjulian.dev@gmail.com>
2023-08-07 20:53:59 +0200

    wip routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'chats',
+    loadChildren: () => import('./pages/chats/chats.module').then(m => m.ChatsModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
