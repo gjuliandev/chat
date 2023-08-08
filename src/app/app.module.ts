51a5cdb98b52c360044e816498498d832554d74f
Gabriel <gjulian.dev@gmail.com>
2023-08-08 09:37:44 +0200

    profile layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
@@ -24,6 +25,7 @@ import { LayoutModule } from './layout/layout.module';
       provideAuth(() => getAuth()),
       provideDatabase(() => getDatabase()),
       provideFirestore(() => getFirestore()),
+      provideStorage( () => getStorage()),
     LayoutModule
   ],
   providers: [],
--- src/app/pages/profile/profile/profile.component.ts
+++ src/app/pages/profile/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { FirebaseService } from 'src/app/providers/firebase.service';
-
+import { Storage, getDownloadURL, list, listAll, ref, uploadBytes} from '@angular/fire/storage';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -8,13 +9,65 @@ import { FirebaseService } from 'src/app/providers/firebase.service';
 })
 export class ProfileComponent implements OnInit {
 
+  imagenesCargadas: Array<any> = [];
+  tmpImageUrl: string = '';
+  tmpName: string = '';
+
   user: any;
+  profileForm: FormGroup = Object.create(null);
 
   constructor(
-    private firebaseService: FirebaseService
+    private firebaseService: FirebaseService,
+    private storage: Storage
   ) { }
 
   ngOnInit(): void {
   }
+
+  save() {
+    
+  }
+
+  uploadFile(e: any) {
+    const file = e.target.files[0];
+    this.tmpImageUrl = '';
+    const imageRef = ref(this.storage, `images/${file.name}`);
+    uploadBytes( imageRef, file).then( x => {
+      this.tmpName = x.metadata.name;
+      this.getImage();
+    })
+    .catch( error => {
+      console.log(error)
+    });
+  }
+
+  getImage() {
+    this.imagenesCargadas = [];
+
+    const imageRef = ref(this.storage, 'images');
+    
+    listAll(imageRef).then( async  images => {
+      for (let image of images.items) {
+        const url = await getDownloadURL(image);
+        this.imagenesCargadas.push(url);
+      }
+      this.selectImage();
+    });
+  }
+
+  selectImage() {
+    if (this.tmpName === '') {
+      this.tmpImageUrl = this.imagenesCargadas[0]
+    } else {
+      this.imagenesCargadas.forEach( (item: string) => {
+        if(item.includes(this.tmpName) ) {
+          this.tmpImageUrl = item;
+          return;
+        };
+      
+      })
+    }
+  }
+
 
 }
